8e1b38391bbe1d7eea571bf015b4e5c20d7b4809
Vic <anis.benziane@gmail.com>
2022-06-18 01:32:22 +0200

    front: lint fix

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
